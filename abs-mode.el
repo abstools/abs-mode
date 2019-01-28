e65eb5af2e744d56d442d475bf413e445f451c88
Lars Tveito <larstvei@ifi.uio.no>
2019-01-28 17:23:33 +0100

    Call `start_console` directly

--- abs-mode.el
+++ abs-mode.el
@@ -577,48 +577,25 @@ can edit it before compilation starts."
               (goto-char (point-max))
               (insert "frew start .")
               (comint-send-input)))
-    (`erlang (let* ((erlang-buffer (progn
-                                     (when (get-buffer "*erlang*")
-                                       (kill-buffer (get-buffer "*erlang*")))
-                                     (save-excursion
-                                       ;; don't propagate
-                                       ;; interactive args, if any
-                                       (inferior-erlang nil))
-                                     (get-buffer "*erlang*")))
-                    (erlang-dir (concat (file-name-directory (buffer-file-name))
-                                        "gen/erl/absmodel"))
-                    (erlang-code-path
-                     (directory-files-recursively erlang-dir "^ebin$" t))
-                    ;; (module (abs--guess-module))
-                    (clock-limit abs-clock-limit)
-                    (port abs-local-port)
-                    (influxdb-enable abs-influxdb-enable)
-                    (influxdb-url abs-influxdb-url)
-                    (influxdb-db abs-influxdb-db))
-               (with-current-buffer erlang-buffer
-                 (comint-send-string
-                  erlang-buffer (concat "cd (\"" erlang-dir "\").\n"))
-                 (comint-send-string
-                  erlang-buffer (concat "code:add_paths([\""
-                                        (cl-reduce
-                                         (lambda (p1 p2) (concat p1 "\", \"" p2))
-                                         erlang-code-path)
-                                        "\"]).\n"))
-                 (comint-send-string erlang-buffer "make:all([load]).\n")
-                 (comint-send-string
-                  erlang-buffer (concat "runtime:start(\""
-                                        (when clock-limit (format " -l %d " clock-limit))
-                                        (when port (format " -p %d " port))
-                                        (when influxdb-enable (format " -i "))
-                                        (when influxdb-url (format " -u %s " influxdb-url))
-                                        (when influxdb-db (format " -d %s " influxdb-db))
-                                        ;; FIXME: reinstate `module' arg
-                                        ;; once abs--guess-module doesn't
-                                        ;; pick a module w/o main block
-
-                                        ;; module
-                                        "\").\n")))
-               (pop-to-buffer erlang-buffer)))
+    (`erlang (let* ((dirname (file-name-directory (buffer-file-name)))
+                    (script (car (directory-files-recursively dirname "^start_console$" t)))
+                    (args (concat
+                           (when abs-clock-limit (format " -l %d " abs-clock-limit))
+                           (when abs-local-port (format " -p %d " abs-local-port))
+                           (when abs-influxdb-enable (format " -i "))
+                           (when abs-influxdb-url (format " -u %s " abs-influxdb-url))
+                           (when abs-influxdb-db (format " -d %s " abs-influxdb-db))
+                           ;; FIXME: reinstate `module' arg
+                           ;; once abs--guess-module doesn't
+                           ;; pick a module w/o main block
+                           ))
+                    (erl-command (concat script args)))
+               (when (get-buffer "*erlang*")
+                 (kill-buffer (get-buffer "*erlang*")))
+               (save-excursion
+                 ;; don't propagate
+                 ;; interactive args, if any
+                 (inferior-erlang erl-command))))
     (`java (let* ((module (abs--guess-module))
                   (java-buffer (get-buffer-create (concat "*abs java " module "*")))
                   (command (concat "java -cp gen:" abs-java-classpath
