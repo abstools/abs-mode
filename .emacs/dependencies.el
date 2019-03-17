573e78aa2ab32610af4dbc5fbc87a4a1d11c8b97
Rudi Schlatte <rudi@constantly.at>
2019-03-17 22:58:22 +0100

    Try to fix linting ob-abs.el on emacs 25
    - This needs a newer version of org-mode than installed on emacs 25 by default

--- .emacs/dependencies.el
+++ .emacs/dependencies.el
@@ -1,19 +1,15 @@
 ;;; dependencies.el - project specific package dependencies
 
-(use-package elisp-lint
-  ;;:pin melpa-stable
-  :ensure t)
+(use-package elisp-lint :ensure t)
 
 (use-package cc-mode)
 
 (use-package cc-langs)
 
-(use-package erlang-start
-  ;;:pin melpa-stable
-  :ensure erlang)
+(use-package erlang-start :ensure erlang)
 
-(use-package cov
-  ;;:pin melpa-stable
-  :ensure t)
+(use-package cov :ensure t)
+
+(use-package org :ensure t)
 
 ;;; dependencies.el ends here
